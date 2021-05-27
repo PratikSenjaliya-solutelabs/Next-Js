b92d80e3f7e62e4fd4be923098aec7ec9092cbbc
PratikSenjaliya-solutelabs <78605614+PratikSenjaliya-solutelabs@users.noreply.github.com>
2021-05-27 17:44:36 +0530

    Second Commit

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       <meta name="keywords" content="ninjas"></meta>
     </Head>
     <div >
-     <h1 className={styles.title}>HELLO WORLD</h1>
+     <h1 className={styles.title}>HELLO WORLDS</h1>
      <p className={styles.text}>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
      Lorem Ipsum has been the industry's standard dummy text ever since the
      1500s, when an unknown printer took a galley of type and scrambled it to
